8401a6a5de127140e7b102167c9ca524fe9f833f
nonolol <29190026@qq.com>
2016-07-01 12:17:07 +0800

    第一版本提交

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smooth = 6f;
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPosition, smooth * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    Animator playerAnim;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Animation(h, v);
+        Tunning();
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Animation(float h,float v)
+    {
+        var isWalking = h != 0 || v != 0;
+        playerAnim.SetBool("IsWalking", isWalking);
+    }
+
+    void Tunning()
+    {
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if(Physics.Raycast(camRay,out floorHit, camRayLength,floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            var newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+}
